b35e5d7b6d70d45ace111931b6fec4df5fceda18
Reysanor <michalreyman@gmail.com>
2019-01-13 23:15:24 +0100

    first commit

---
+++ magazynV3/src/main/java/personal/demo/MagazynV3Application.java
@@ -0,0 +1,14 @@
+package personal.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MagazynV3Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MagazynV3Application.class, args);
+    }
+
+}
+
---
+++ magazynV3/src/test/java/personal/demo/MagazynV3ApplicationTests.java
@@ -0,0 +1,17 @@
+package personal.demo;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class MagazynV3ApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
+

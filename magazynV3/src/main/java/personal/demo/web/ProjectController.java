04f051c02d04a65cdcad9315477f1512e5cfa0bb
Reysanor <michalreyman@gmail.com>
2019-01-16 22:53:51 +0100

    validate

--- magazynV3/src/main/java/personal/demo/web/ProjectController.java
+++ magazynV3/src/main/java/personal/demo/web/ProjectController.java
@@ -4,9 +4,12 @@ package personal.demo.web;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
 import personal.demo.domain.Project;
 import personal.demo.services.ProjectService;
+
+import javax.validation.Valid;
 
 @RestController
 @RequestMapping("/api/project")
@@ -15,8 +18,13 @@ public class ProjectController {
     @Autowired
     private ProjectService projectService;
 
-    @PostMapping("")
-    public ResponseEntity<Project> createNewProject(@RequestBody Project project){
+    @PostMapping("") //dodanie @Valid formatuje informacje z walidacji, natomist BindingResult sprawdza czy był błędy
+                     // i zmienia tresc
+    public ResponseEntity<?> createNewProject(@Valid @RequestBody Project project, BindingResult result){
+
+        if (result.hasErrors()){
+            return new ResponseEntity<String>("invalid object Object", HttpStatus.BAD_REQUEST);
+        }
         Project project1 = projectService.saveOrUpdateRepository(project);
         return new ResponseEntity<Project>(project,HttpStatus.CREATED);
 
